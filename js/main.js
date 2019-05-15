455aa69f42d0aa479d27ac2bebfdb2c1beecd7a1
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-05-15 13:15:12 +0200

    Dwie nawigacje

--- js/main.js
+++ js/main.js
@@ -1,33 +1,42 @@
 (function () {
     "use strict";
 
-    var $navLinks = document.querySelectorAll('.nav-link');
-    var $navPage = document.querySelectorAll('.nav-page');
-    var cssClassActive = 'active';
+    var initNav = function ($navSections){
+        var $navLinks = document.querySelectorAll('.nav-link');
+        var $navPage = document.querySelectorAll('.nav-page');
+        var cssClassActive = 'active';
 
-    var onNavClick = function (evt){
-        evt.preventDefault();
-        var targetContentId = this.getAttribute('href').replace('#', '');
+        var onNavClick = function (evt){
+            evt.preventDefault();
+            var targetContentId = this.getAttribute('href').replace('#', '');
 
-        for (var i = $navPage.length - 1; i >= 0; i--) {
-            let $content = $navPage[i];
+            for (var i = $navPage.length - 1; i >= 0; i--) {
+                let $content = $navPage[i];
 
-            if ($content.id === targetContentId) {
-                $content.classList.add(cssClassActive);
-            } else {
-                $content.classList.remove(cssClassActive);
+                if ($content.id === targetContentId) {
+                    $content.classList.add(cssClassActive);
+                } else {
+                    $content.classList.remove(cssClassActive);
+                }
             }
-        }
+
+            for (var i = $navLinks.length - 1; i >= 0; i--) {
+                $navLinks[i].classList.remove(cssClassActive);
+            }
+
+            this.classList.add(cssClassActive);
+        };
 
         for (var i = $navLinks.length - 1; i >= 0; i--) {
-            $navLinks[i].classList.remove(cssClassActive);
+            $navLinks[i].addEventListener('click', onNavClick);
         }
-
-        this.classList.add(cssClassActive);
     };
 
-    for (var i = $navLinks.length - 1; i >= 0; i--) {
-        $navLinks[i].addEventListener('click', onNavClick);
+    var $navSections = document.querySelectorAll('.nav-section');
+
+    for (var i = $navSections.length - 1; i >= 0; i--) {
+        initNav($navSections[i]);
     }
+
     
 })();
